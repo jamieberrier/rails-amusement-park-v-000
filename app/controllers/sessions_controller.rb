fefc741ce7f0247cc93bb918e3906713219de9ea
Jamie Berrier <berrier.jamie@gmail.com>
2020-01-10 21:57:31 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,17 @@ class SessionsController < ApplicationController
     binding.pry
     # try is an ActiveSupport method. object.try(:some_method) means if object != nil then object.some_method else nil end.
     #authenticated = @user.try(:authenticate, params[:user][:password])
+    if @user.nil?
 
-    # Users should not be able to log in if they enter an incorrect password. Just redirect them back to the login page.
-    if @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user.id)
     else
-      flash[:error] = "Invalid Password"
-      redirect_to signin_path
+    # Users should not be able to log in if they enter an incorrect password. Just redirect them back to the login page.
+      if @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user.id)
+      else
+        flash[:error] = "Invalid Password"
+        redirect_to signin_path
+      end
     end
   end
 
