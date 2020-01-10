10289ebfb419b6e0069554e828983d24bd51e398
Jamie Berrier <berrier.jamie@gmail.com>
2020-01-10 21:39:30 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,7 +4,7 @@ class RidesController < ApplicationController
   def create
     @ride = Ride.new(user_id: params[:user_id], attraction_id: params[:attraction_id])
     @result = @ride.take_ride 
-    flash[@result.keys[0].to_s] = @result.values[0]
+    flash[@result.keys[0]] = @result.values[0]
     redirect_to user_path(current_user.id)
   end
 end
