a94a87d2349b0accc39bea9c218b7cc3fc2cba65
Jamie Berrier <berrier.jamie@gmail.com>
2020-01-10 21:42:18 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,5 +1,4 @@
 class RidesController < ApplicationController
-  #before_action :require_logged_in
 
   def create
     @ride = Ride.new(user_id: params[:user_id], attraction_id: params[:attraction_id])
