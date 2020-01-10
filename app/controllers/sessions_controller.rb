72c8f9ae64946afbbf7616cc2da5743ae09b9ab4
Jamie Berrier <berrier.jamie@gmail.com>
2020-01-10 21:48:37 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:user][:name])
-    binding.pry
+    #binding.pry
     # try is an ActiveSupport method. object.try(:some_method) means if object != nil then object.some_method else nil end.
     authenticated = @user.try(:authenticate, params[:user][:password])
     # Users should not be able to log in if they enter an incorrect password. Just redirect them back to the login page.
-    flash[:error] = "Invalid Log In Credentials"
+    #flash[:error] = "Invalid Log In Credentials"
 
     return redirect_to signin_path unless authenticated
 
