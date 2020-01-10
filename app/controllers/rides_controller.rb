575552a0480e9e025e464aed12caa5c2eb479f2a
Jamie Berrier <berrier.jamie@gmail.com>
2020-01-10 21:06:51 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -5,7 +5,7 @@ class RidesController < ApplicationController
     @ride = Ride.new(user_id: params[:user_id], attraction_id: params[:attraction_id])
     @message = @ride.take_ride
     binding.pry
-    flash[@message.keys[0].to_s] = @message.values[0]
+    flash[:error] = @message.values[0]
     redirect_to user_path(current_user.id)
   end
 end
