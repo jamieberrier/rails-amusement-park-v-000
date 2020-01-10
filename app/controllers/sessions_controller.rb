84bccc0fc63514db8e96623456736d6101835929
Jamie Berrier <berrier.jamie@gmail.com>
2020-01-10 21:46:54 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry
     @user = User.find_by(name: params[:user][:name])
+    binding.pry
     # try is an ActiveSupport method. object.try(:some_method) means if object != nil then object.some_method else nil end.
     authenticated = @user.try(:authenticate, params[:user][:password])
     # Users should not be able to log in if they enter an incorrect password. Just redirect them back to the login page.
